8708eaf8a0143a2d5ec042eaa2679532b5728b16
zunedisinambela <zunedisinambela@gmail.com>
2022-06-15 02:42:33 +0700

    Make Model

---
+++ om_hospital/models/patient.py
@@ -0,0 +1,15 @@
+from odoo import api, fields, models
+
+
+class HospitalPatient(models.Model):
+    _name = "hospital.patient"
+    _description = "Hospital Patient"
+
+    name = fields.Char(string='Name', required=True)
+    age = fields.Char(string='Age')
+    gender = fields.Selection([
+        ('male', 'Male'),
+        ('female', 'Female'),
+        ('other', 'other'),
+    ], required=True, default='male')
+    note = fields.Text(string='Description')
