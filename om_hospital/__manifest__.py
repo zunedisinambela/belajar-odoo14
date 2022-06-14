9bfc7807d2ac5d13c799dad41c73bbcd13157fd2
zunedisinambela <zunedisinambela@gmail.com>
2022-06-15 02:26:22 +0700

    Make Configuration

---
+++ om_hospital/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Hospital Management',
+    'version': '1.o',
+    'summary': 'Hospital Management Software',
+    'sequence': -100,
+    'description': """Hospital Management Software""",
+    'category': 'Productivity',
+    'website': 'https://www.odoomates.tech',
+    'license': 'LGPL-3',
+    'depends': [],
+    'data': [],
+    'qweb': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
