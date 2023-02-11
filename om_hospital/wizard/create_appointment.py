e44c3719d7035a16146f08dbc3a4fa6caf9ad7e7
zunedisinambela <zunedisinambela@gmail.com>
2023-02-11 07:22:59 +0700

    Create Record From Code In Odoo Add Record To Model From Wizard In Odoo

--- om_hospital/wizard/create_appointment.py
+++ om_hospital/wizard/create_appointment.py
@@ -5,8 +5,13 @@ class CreateAppointmentWizard(models.TransientModel):
     _name = "create.appointment.wizard"
     _description = "Create Appointment Wizard"
 
-    name = fields.Char(string='Name', required=True)
+    date_appointment = fields.Date(string='Date', required=True)
     patient_id = fields.Many2one('hospital.patient', string="Patient", required=True)
 
     def action_create_appointment(self):
         print("Button is Clicked")
+        vals = {
+            'patient_id': self.patient_id.id,
+            'date_appointment': self.date_appointment,
+        }
+        self.env['hospital.appointment'].create(vals)
